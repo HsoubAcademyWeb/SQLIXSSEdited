97474d6f74ea72d52bdec33ea074964b6de59967
HsoubAcademyWeb <56909275+HsoubAcademyWeb@users.noreply.github.com>
2020-03-02 05:19:10 +0200

    Update db_member.sql

--- db_member.sql
+++ db_member.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2020 at 01:37 PM
+-- Generation Time: Mar 02, 2020 at 04:18 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -37,6 +37,51 @@ CREATE TABLE `admin` (
   `lastname` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `comment_text` text CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`id`, `username`, `comment_text`) VALUES
+(1, 'Entesar', 'تطبيق رائع'),
+(7, 'Web Designer', 'تصميمه جميل');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id`, `username`, `password`, `email`, `firstname`, `lastname`) VALUES
+(1, 'hsoub', '123456', 'hsoubacademy@gmail.com', 'hsoub', 'academy'),
+(4, 'hsoub2', 'e10adc3949ba59abbe56e057f20f883e', 'HsoubWeb@gmail.com', 'hsoub', 'web'),
+(5, 'hsoub3', '123456', 'course@gmail.com', 'hsoub', 'course'),
+(6, 'Entesat', 'e10adc3949ba59abbe56e057f20f883e', 'entesar@gmail.com', 'Entesar', 'ElBanna');
+
 --
 -- Indexes for dumped tables
 --
@@ -48,6 +93,18 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -56,6 +113,18 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
